f85a2a06e17a5b7a96c4ebb1e9e0394ab3390d57
Joao-error404 <joaoo.oliveiraneto@gmail.com>
2026-08-21 13:50:32 -0300

    chore: project folder structure architecture setup

---
+++ FinTrack/FinTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinTrack
+//
+//  Created by j.de.oliveira.neto on 21/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinTrack/FinTrack/FinTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinTrackApp.swift
+//  FinTrack
+//
+//  Created by j.de.oliveira.neto on 21/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FinTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
