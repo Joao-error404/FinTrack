9f3df4d9bd41161a55340a27118c6cfb6ffc8177
Breno <brenofreitas28f@gmail.com>
2026-08-25 13:57:53 -0300

    feat: navbar

--- FinTrack/FinTrack/FinTrackApp.swift
+++ FinTrack/FinTrack/FinTrackApp.swift
@@ -10,11 +10,16 @@ import SwiftUI
 @main
 struct FinTrackApp: App {
     
-    @State private var selectedTab = "home"
+    @State private var selectedTab = "dashboard"
     
     var body: some Scene {
         WindowGroup {
             TabView (selection: $selectedTab) {
+                FinanceDashboardView()
+                    .tabItem {
+                        Label("Dashboard", systemImage: "house.fill")
+                    }
+                    .tag("dashboard")
                 ExpensesView()
                     .tabItem {
                         Label("Transactions", systemImage: "wallet.bifold")
