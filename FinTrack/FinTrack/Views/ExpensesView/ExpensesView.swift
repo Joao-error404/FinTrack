46cc49c4fae8ec163aef31713d1a2c1f675d9307
Breno <brenofreitas28f@gmail.com>
2026-08-26 11:07:07 -0300

    feat: transactions summary card componentized

--- FinTrack/FinTrack/Views/ExpensesView/ExpensesView.swift
+++ FinTrack/FinTrack/Views/ExpensesView/ExpensesView.swift
@@ -7,33 +7,6 @@
 
 import SwiftUI
 
-struct FrameModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding(.horizontal, 10)
-            .frame(maxWidth: .infinity, minHeight: 80, alignment: .leading)
-            .background {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(Color("Surface"))
-                    .shadow(
-                        color: .black.opacity(0.1),
-                        radius: 6
-                    )
-            }
-            .overlay {
-                RoundedRectangle(cornerRadius: 25)
-                    .stroke(Color("Border"), lineWidth: 1)
-            }
-            
-    }
-}
-
-extension View {
-    func frameStyle() -> some View {
-        modifier(FrameModifier())
-    }
-}
-
 struct ExpensesView: View {
     @State private var totalEarnings = 0.0
     @State private var totalExpenses = 0.0
@@ -46,19 +19,17 @@ struct ExpensesView: View {
     ]
     
     let transactions = [
-        FinancialTransaction(title: "Teste", amount: 20.0, type: TransactionType.expense, category: TransactionCategory.education),
-        FinancialTransaction(title: "Teste 2", amount: 30.0, type: TransactionType.income, category: TransactionCategory.education)
+        FinancialTransaction(
+            title: "Teste",
+            amount: 20.0,
+            type: TransactionType.expense,
+            category: TransactionCategory.education),
+        FinancialTransaction(
+            title: "Teste 2",
+            amount: 30.0,
+            type: TransactionType.income,
+            category: TransactionCategory.education)
     ]
-    
-    func formattedNumber (_ number: Double) -> String {
-        let currencyCode = Locale.current.currency?.identifier ?? "BRL"
-        
-        if number >= 100_000 {
-            return String(format: "%.0fK", number / 1_000)
-        }
-        
-        return number.formatted(.currency(code: currencyCode))
-    }
     
     var body: some View {
         NavigationStack {
@@ -70,57 +41,19 @@ struct ExpensesView: View {
                     TransactionsHeader(transactionCount: transactions.count)
                     
                     VStack {
-                        HStack(spacing: 20){
-                            HStack {
-                                ZStack {
-                                    Circle()
-                                        .fill(Color("Success").opacity(0.2))
-                                        .frame(width: 30, height: 30)
-                                    
-                                    Image(systemName: "arrow.up.right")
-                                        .font(.system(size: 10))
-                                        .foregroundStyle(Color("Success"))
-                                }
-                                
-                                VStack(alignment: .leading){
-                                    Text("Incomes")
-                                        .font(.caption)
-                                        .foregroundStyle(Color("TextMuted"))
-                                    Text(formattedNumber(totalEarnings))
-                                        .font(.headline)
-                                        .lineLimit(1)
-                                        .foregroundStyle(Color("Foreground"))
-                                        .fontWeight(.bold)
-                                }
-                            }
-                            .frameStyle()
+                        HStack{
+                            TransactionsSummaryCard(
+                                title: "Incomes",
+                                amount: 0.0,
+                                icon: "arrow.up.right",
+                                color: Color("Success"))
                             
-                            HStack{
-                                ZStack {
-                                    Circle()
-                                        .fill(Color("Error").opacity(0.2))
-                                        .frame(width: 30, height: 30)
-                                    
-                                    Image(systemName: "arrow.down.right")
-                                        .font(.system(size: 10))
-                                        .foregroundStyle(Color("Error"))
-                                }
-                                VStack(alignment: .leading){
-                                    Text("Expenses")
-                                        .font(.caption)
-                                        .foregroundStyle(Color("TextMuted"))
-                                    Text(formattedNumber(totalExpenses))
-                                        .font(.headline)
-                                        .lineLimit(1)
-                                        .foregroundStyle(Color("Foreground"))
-                                        .fontWeight(.bold)
-                                    
-                                }
-                            }
-                            .frameStyle()
+                            TransactionsSummaryCard(
+                                title: "Expenses",
+                                amount: 0.0,
+                                icon: "arrow.down.right",
+                                color: Color("Error"))
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
                         
                         HStack(spacing: 0) {
                             ForEach(tabs.indices, id: \.self) { index in
